ab35fc630643ecd762d6ad08a5f0478bacf61ba3
Nishtha Kohli <89636008+Nishthakohli1637@users.noreply.github.com>
2024-03-21 17:35:44 +0530

    Update rat maze.cpp

--- rat maze.cpp
+++ rat maze.cpp
@@ -37,3 +37,45 @@ int main(){
     maze_path(v, n, 0, 0, solution);
     return 0;
 }
+
+
+APPROACH 2
+    class Solution{
+    void solve(int i, int j, vector<vector<int>> &m, int n, vector<string> &ans, string move, vector<vector<int>> &vis){
+        if(i==n-1 && j==n-1){
+            ans.push_back(move);
+            return;
+        }
+        if(i+1<n && !vis[i+1][j] && m[i+1][j]==1){
+            vis[i+1][j]=1;
+            solve(i+1, j, m, n, ans, move+"D", vis);
+            vis[i+1][j]=0;
+        }
+        if(j-1>=0 && !vis[i][j-1] && m[i][j-1]==1){
+            vis[i][j-1]=1;
+            solve(i, j-1, m, n, ans, move+"L", vis);
+            vis[i][j-1]=0;
+        }
+        if(j+1<n && !vis[i][j+1] && m[i][j+1]==1){
+            vis[i][j+1]=1;
+            solve(i, j+1, m, n, ans, move+"R", vis);
+            vis[i][j+1]=0;
+        }
+        if(i-1>=0 && !vis[i-1][j] && m[i-1][j]==1){
+            vis[i-1][j]=1;
+            solve(i-1, j, m, n, ans, move+"U", vis);
+            vis[i-1][j]=0;
+        }
+    }
+    public:
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        // Your code goes here
+        vector<string> ans;
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        if(m[0][0]==1){
+            vis[0][0]=1;
+            solve(0, 0, m, n, ans, "", vis);
+        }
+        return ans;
+    }
+};
